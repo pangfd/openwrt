4aece306887374d334101f9288f50bf5aa3c4000
LGA1150 <dqfext@gmail.com>
2020-08-13 14:37:57 +0800

    mvebu: hack: switch to neon and disable 370/xp

--- target/linux/mvebu/image/cortexa9.mk
+++ target/linux/mvebu/image/cortexa9.mk
@@ -26,7 +26,6 @@ define Device/buffalo_ls421de
     kmod-linkstation-poweroff kmod-md-raid0 kmod-md-raid1 kmod-md-mod \
     kmod-fs-xfs mkf2fs e2fsprogs partx-utils
 endef
-TARGET_DEVICES += buffalo_ls421de
 
 define Device/cznic_turris-omnia
   DEVICE_VENDOR := CZ.NIC
@@ -55,7 +54,6 @@ define Device/globalscale_mirabox
   SOC := armada-370
   SUPPORTED_DEVICES += mirabox
 endef
-TARGET_DEVICES += globalscale_mirabox
 
 define Device/kobol_helios4
   DEVICE_VENDOR := Kobol
@@ -122,7 +120,6 @@ define Device/linksys_wrt1900ac-v1
   SUPPORTED_DEVICES += armada-xp-linksys-mamba linksys,mamba
   DEFAULT := n
 endef
-TARGET_DEVICES += linksys_wrt1900ac-v1
 
 define Device/linksys_wrt1900ac-v2
   $(call Device/linksys)
@@ -171,7 +168,6 @@ define Device/marvell_a370-db
   DEVICE_DTS := armada-370-db
   SUPPORTED_DEVICES += armada-370-db
 endef
-TARGET_DEVICES += marvell_a370-db
 
 define Device/marvell_a370-rd
   $(Device/NAND-512K)
@@ -180,7 +176,6 @@ define Device/marvell_a370-rd
   DEVICE_DTS := armada-370-rd
   SUPPORTED_DEVICES += armada-370-rd
 endef
-TARGET_DEVICES += marvell_a370-rd
 
 define Device/marvell_a385-db-ap
   $(Device/NAND-256K)
@@ -212,7 +207,6 @@ define Device/marvell_axp-db
   DEVICE_DTS := armada-xp-db
   SUPPORTED_DEVICES += armada-xp-db
 endef
-TARGET_DEVICES += marvell_axp-db
 
 define Device/marvell_axp-gp
   $(Device/NAND-512K)
@@ -221,7 +215,6 @@ define Device/marvell_axp-gp
   DEVICE_DTS := armada-xp-gp
   SUPPORTED_DEVICES += armada-xp-gp
 endef
-TARGET_DEVICES += marvell_axp-gp
 
 define Device/plathome_openblocks-ax3-4
   DEVICE_VENDOR := Plat'Home
@@ -234,7 +227,6 @@ define Device/plathome_openblocks-ax3-4
   IMAGES += factory.img
   IMAGE/factory.img := append-kernel | pad-to $$(BLOCKSIZE) | append-ubi
 endef
-TARGET_DEVICES += plathome_openblocks-ax3-4
 
 define Device/solidrun_clearfog-base-a1
   DEVICE_VENDOR := SolidRun
